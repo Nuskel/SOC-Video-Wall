2625eda18aeac23e8fe52aee4436745f2d94a741
Nuskel <noablas@aol.de>
2022-06-24 13:23:05 +0200

    AppComponent - initial load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ControlService } from 'shared/service/control.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  constructor(private control: ControlService) {}
+  
+  ngOnInit() {
+    this.control.initLoad();
+  }
 
 }
